e3f25d09ac4b32d4c624085b221d92b65e602ec5
evgen100692-del <evgen100692@gmail.com>
2026-06-04 14:52:08 +0300

    Доработка модалок

--- js/utils.js
+++ js/utils.js
@@ -151,18 +151,11 @@ function closeModal(id) {
   if (overlay) overlay.classList.remove('open');
 }
 
-// Close on backdrop click
-document.addEventListener('click', e => {
-  if (e.target.classList.contains('modal-overlay')) {
-    e.target.classList.remove('open');
-  }
-});
-
 // Close on Escape
 document.addEventListener('keydown', e => {
   if (e.key === 'Escape') {
     document.querySelectorAll('.modal-overlay.open').forEach(m => m.classList.remove('open'));
-  }
+  } 
 });
 
 // ---- Confirm dialog ----
